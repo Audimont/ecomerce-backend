91924e0a2393d03122c554b2034a7d2842905da9
Marc Dechand <mdelariva@zintro.com>
2024-11-12 18:15:57 -0300

    saving products in db

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,14 @@
+import { Transform } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
   name: string;
+
+  @IsString()
   description: string;
+  
+  @Transform(({ value }) => parseFloat(value))
+  @IsNumber({ maxDecimalPlaces: 2 })
   price: number;
 }
